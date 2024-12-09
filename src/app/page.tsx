06445e3351c05edc17ef064a29bf73e0e80f47b9
Titas Virbickas <147817617+titusvi@users.noreply.github.com>
2024-12-09 08:28:40 +0200

    fix: remove Video component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
 import Testimonials from "@/components/Testimonials";
-import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -23,7 +22,6 @@ export default function Home() {
       <ScrollUp />
       <Hero />
       <Features />
-      <Video />
       <Brands />
       <AboutSectionOne />
       <AboutSectionTwo />
