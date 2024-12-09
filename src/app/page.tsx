8a38ba5472acd20e92d61f573df6632acc82d634
Titas Virbickas <titasv@proton.me>
2024-12-09 09:08:05 +0200

    fix: remove unneeded parts of the application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
-import Brands from "@/components/Brands";
-import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
-import Testimonials from "@/components/Testimonials";
+
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -19,16 +14,10 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <ScrollUp />
       <Hero />
       <Features />
-      <Brands />
       <AboutSectionOne />
       <AboutSectionTwo />
-      <Testimonials />
-      <Pricing />
-      <Blog />
-      <Contact />
     </>
   );
 }
