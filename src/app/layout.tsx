d0452a9deffd1e036c5aa7e07e3902e374414be8
Titas Virbickas <147817617+titusvi@users.noreply.github.com>
2024-12-09 09:08:05 +0200

    fix: remove unneeded parts of the application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
 import "../styles/index.css";
 
@@ -26,7 +25,6 @@ export default function RootLayout({
           <Header />
           {children}
           <Footer />
-          <ScrollToTop />
         </Providers>
       </body>
     </html>
