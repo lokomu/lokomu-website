12e063a9d8eff583153e1e4bf3ae1cbd5da0ee81
Titas Virbickas <titasv@proton.me>
2024-12-11 09:16:44 +0200

    fix: import images rather than setting paths

--- src/components/About/AboutSectionOne.tsx
+++ src/components/About/AboutSectionOne.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import SectionTitle from "../Common/SectionTitle";
 
+import AboutImage from "~/public/images/about/about-image.svg";
+import AboutDarkImage from "~/public/images/about/about-image-dark.svg";
+
 const checkIcon = (
   <svg width="16" height="13" viewBox="0 0 16 13" className="fill-current">
     <path d="M5.8535 12.6631C5.65824 12.8584 5.34166 12.8584 5.1464 12.6631L0.678505 8.1952C0.483242 7.99994 0.483242 7.68336 0.678505 7.4881L2.32921 5.83739C2.52467 5.64193 2.84166 5.64216 3.03684 5.83791L5.14622 7.95354C5.34147 8.14936 5.65859 8.14952 5.85403 7.95388L13.3797 0.420561C13.575 0.22513 13.8917 0.225051 14.087 0.420383L15.7381 2.07143C15.9333 2.26669 15.9333 2.58327 15.7381 2.77854L5.8535 12.6631Z" />
@@ -56,13 +59,13 @@ const AboutSectionOne = () => {
             <div className="w-full px-4 lg:w-1/2">
               <div className="relative mx-auto aspect-[25/24] max-w-[500px] lg:mr-0">
                 <Image
-                  src="~/public/images/about/about-image.svg"
+                  src={AboutImage}
                   alt="about-image"
                   fill
                   className="mx-auto max-w-full drop-shadow-three dark:hidden dark:drop-shadow-none lg:mr-0"
                 />
                 <Image
-                  src="~/public/images/about/about-image-dark.svg"
+                  src={AboutDarkImage}
                   alt="about-image"
                   fill
                   className="mx-auto hidden max-w-full drop-shadow-three dark:block dark:drop-shadow-none lg:mr-0"
