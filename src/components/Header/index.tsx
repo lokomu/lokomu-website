ee5727d14eb56401d5ce3eded30bfd49ed985b1c
Titas Virbickas <147817617+titusvi@users.noreply.github.com>
2024-12-11 07:47:02 +0200

    fix: make images use absolute paths

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -46,14 +46,14 @@ const Header = () => {
                 } `}
               >
                 <Image
-                  src="/images/logo/logo-2.svg"
+                  src="~/public/images/logo/logo-2.svg"
                   alt="logo"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/logo.svg"
+                  src="~/public/images/logo/logo.svg"
                   alt="logo"
                   width={140}
                   height={30}
