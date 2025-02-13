a3cc3242fdb4a952ac78e8d0a15c556e6d3d56ef
Titas Virbickas <147817617+titusvi@users.noreply.github.com>
2025-02-13 21:44:54 +0200

    fix(ui): restyle the Hero component to use a background

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,43 +1,35 @@
-import Link from "next/link";
+import { BlurredBackground } from "@/components/Common/BlurredBackground";
+import { LearnMoreButton } from "../Common/LearnMoreButton";
 
-const Hero = () => {
+export function Hero() {
   return (
-    <>
-      <section
-        id="home"
-        className="relative z-10 overflow-hidden bg-white pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
+    <section id="home" className="z-10 overflow-hidden">
+      <BlurredBackground
+        src={"/images/landing.webp"}
+        fullHeight
+        blur={10}
+        shade="var(--black-a8)"
+        bgPosition="center"
       >
-        <div className="container">
+        <div className="flex items-center justify-center text-center">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
-              <div className="mx-auto max-w-[800px] text-center">
-                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Local community app
+              <div className="mx-auto max-w-[800px] items-center justify-center text-center">
+                <h1 className="mb-6 text-4xl font-extrabold tracking-tight text-white sm:text-5xl md:text-6xl">
+                  <span className="animate-fade-in bg-gradient-to-r from-green-400 via-purple-400 to-blue-400 bg-clip-text text-transparent">
+                    Gather. Share. Connect
+                  </span>
                 </h1>
-                <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  This is the description.
+                <p className="mb-12 text-base !leading-relaxed text-gray-200 dark:text-body-color-dark sm:text-lg md:text-xl">
+                  Gather with other students. Share your things. Connect as a
+                  community
                 </p>
-                <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-                  <Link
-                    href="https://github.com/lokomu"
-                    className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
-                  >
-                    Support
-                  </Link>
-                  <Link
-                    href="https://github.com/lokomu"
-                    className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
-                  >
-                    Star on GitHub
-                  </Link>
-                </div>
+                <LearnMoreButton />
               </div>
             </div>
           </div>
         </div>
-      </section>
-    </>
+      </BlurredBackground>
+    </section>
   );
-};
-
-export default Hero;
+}
