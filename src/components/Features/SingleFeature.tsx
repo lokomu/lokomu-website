0d9d9a7580e72ddb7f2e99318438c028c2c1ee3d
Titas Virbickas <147817617+titusvi@users.noreply.github.com>
2025-02-13 21:53:53 +0200

    fix(ui): restyle the SingleFeature component to center the content

--- src/components/Features/SingleFeature.tsx
+++ src/components/Features/SingleFeature.tsx
@@ -4,9 +4,11 @@ const SingleFeature = ({ feature }: { feature: Feature }) => {
   const { icon, title, paragraph } = feature;
   return (
     <div className="w-full">
-      <div className="wow fadeInUp" data-wow-delay=".15s">
-        <div className="mb-10 flex h-[70px] w-[70px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary">
-          {icon}
+      <div className="wow fadeInUp text-center" data-wow-delay=".15s">
+        <div className="flex justify-center">
+          <div className="mb-10 flex h-20 w-20 items-center justify-center rounded-full bg-primary bg-opacity-10 p-3 text-primary">
+            {icon}
+          </div>
         </div>
         <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
           {title}
