b291542ea66f904719352c51aa627135906df03a
Titas Virbickas <147817617+titusvi@users.noreply.github.com>
2025-02-13 22:04:23 +0200

    fix(ui): remove the paragraph from Features

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -5,11 +5,11 @@ import featuresData from "./featuresData";
 const Features = () => {
   return (
     <>
-      <section id="features" className="py-16 md:py-20 lg:py-28">
+      <section id="features" className="bg-gray-100 py-16 md:py-20 lg:py-28">
         <div className="container">
           <SectionTitle
-            title="Main Features"
-            paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+            title="Get to know other students"
+            paragraph=""
             center
           />
 
