e43c0dbd4c238662d6543acff4b3d8d7bc466dcf
Titas Virbickas <147817617+titusvi@users.noreply.github.com>
2025-02-13 22:04:54 +0200

    fix(ui): change text color of SectionTitle

--- src/components/Common/SectionTitle.tsx
+++ src/components/Common/SectionTitle.tsx
@@ -17,7 +17,7 @@ const SectionTitle = ({
         className={`w-full ${center ? "mx-auto text-center" : ""}`}
         style={{ maxWidth: width, marginBottom: mb }}
       >
-        <h2 className="mb-4 text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[45px]">
+        <h2 className="mb-4 text-3xl font-bold !leading-tight text-primary-900 dark:text-white sm:text-4xl md:text-[45px]">
           {title}
         </h2>
         <p className="text-base !leading-relaxed text-body-color md:text-lg">
